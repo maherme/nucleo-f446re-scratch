8a675be9e00e9d1ff06fa15f865b0f526595381b
maherme <manuel.hernandez.mendez@gmail.com>
2021-06-04 17:40:22 +0200

    Added GPIO_DeInit API.

--- src/gpio_driver.c
+++ src/gpio_driver.c
@@ -48,6 +48,34 @@ void GPIO_Init(GPIO_Handle_t* pGPIOHandle){
 }
 
 void GPIO_DeInit(GPIO_RegDef_t* pGPIOx){
+
+    if(pGPIOx == GPIOA){
+        GPIOA_REG_RESET();
+    }
+    else if(pGPIOx == GPIOB){
+        GPIOB_REG_RESET();
+    }
+    else if(pGPIOx == GPIOC){
+        GPIOC_REG_RESET();
+    }
+    else if(pGPIOx == GPIOD){
+        GPIOD_REG_RESET();
+    }
+    else if(pGPIOx == GPIOE){
+        GPIOE_REG_RESET();
+    }
+    else if(pGPIOx == GPIOF){
+        GPIOF_REG_RESET();
+    }
+    else if(pGPIOx == GPIOG){
+        GPIOG_REG_RESET();
+    }
+    else if(pGPIOx == GPIOH){
+        GPIOH_REG_RESET();
+    }
+    else{
+        /* do nothing */
+    }
 }
 
 void GPIO_PerClkCtrl(GPIO_RegDef_t* pGPIOx, uint8_t en_or_di){
