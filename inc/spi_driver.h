a21cd2dd8b1a63fbdbb18e1692aca97d97d9da08
maherme <manuel.hernandez.mendez@gmail.com>
2021-06-06 19:43:14 +0200

    Added APIs prototypes for SPI driver.

--- inc/spi_driver.h
+++ inc/spi_driver.h
@@ -35,6 +35,105 @@ typedef struct
 {
     SPI_RegDef_t* pSPIx;            /* Base address of the SPIx peripheral */
     SPI_Config_t SPIConfig;         /* SPIx peripheral configuration settings */
-}GPIO_Handle_t;
+}SPI_Handle_t;
+
+/*****************************************************************************************************/
+/*                                       APIs supported                                              */
+/*****************************************************************************************************/
+
+/**
+ * @fn SPI_Init
+ *
+ * @brief function to initialize SPI peripheral.
+ *
+ * @param[in] pSPI_Handle handle structure for the SPI peripheral.
+ *
+ * @return void
+ */
+void SPI_Init(SPI_RegDef_t* pSPI_Handle);
+
+/**
+ *@fn SPI_DeInit
+ *
+ * @brief function to reset all register of a SPI peripheral.
+ *
+ * @param[in] pSPIx the base address of the SPIx peripheral.
+ *
+ * @return void
+ */
+void SPI_DeInit(SPI_RegDef_t* pSPIx);
+
+/**
+ * @fn SPI_PerClkCtrl
+ *
+ * @brief function to control the peripheral clock of the SPI peripheral.
+ *
+ * @param[in] pSPIx the base address of the SPIx peripheral.
+ * @param[in] en_or_di for enable or disable.
+ *
+ * @return void
+ */
+void SPI_PerClkCtrl(SPI_RegDef_t* pSPIx, uint8_t en_or_di);
+
+/**
+ * @fn SPI_SendData
+ *
+ * @brief function to send data.
+ *
+ * @param[in] pSPIx the base address of the SPIx peripheral.
+ * @param[in] pTxBuffer buffer with the data to send.
+ * @param[in] len length of the data to send.
+ *
+ * @return void
+ */
+void SPI_SendData(SPI_RegDef_t* pSPIx, uint8_t* pTxBuffer, uint32_t len);
+
+/**
+ * @fn SPI_ReceiveData
+ *
+ * @brief function to receive data.
+ *
+ * @param[in] pSPIx the base address of the SPIx peripheral.
+ * @param[in] pRxBuffer buffer to store the received data.
+ * @param[in] len length of the data to receive.
+ *
+ * @return void
+ */
+void SPI_ReceiveData(SPI_RegDef_t* pSPIx, uint8_t* pRxBuffer, uint32_t len);
+
+/**
+ * @fn SPI_IRQConfig
+ *
+ * @brief function to configure the IRQ number of the SPI peripheral.
+ *
+ * @param[in] IRQNumber number of the interrupt.
+ * @param[in] en_or_di for enable or disable.
+ *
+ * @return void.
+ */
+void SPI_IRQConfig(uint8_t IRQNumber, uint8_t en_or_di);
+
+/**
+ * @fn SPI_IRQConfig
+ *
+ * @brief function to configure the IRQ number of the SPI peripheral.
+ *
+ * @param[in] IRQNumber number of the interrupt.
+ * @param[in] IRQPriority priority of the interrupt.
+ *
+ * @return void.
+ */
+void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
+
+/**
+ * @fn SPI_IRQHandling
+ *
+ * @brief function to handle the interrupt of the SPI peripheral.
+ *
+ * @param[in] pHandle handle structure to SPI peripheral.
+ *
+ * @return void.
+ */
+void SPI_IRQHandling(SPI_Handle_t* pHandle);
 
 #endif 
