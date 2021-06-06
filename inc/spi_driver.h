ce7e9358c3338dfd73836c4590b959feee6a6b5c
maherme <manuel.hernandez.mendez@gmail.com>
2021-06-06 20:30:41 +0200

    Updated header comment.

--- inc/spi_driver.h
+++ inc/spi_driver.h
@@ -5,6 +5,14 @@
 *       Header file containing the prototypes of the APIs for configuring the SPI peripheral.
 *
 * PUBLIC FUNCTIONS :
+*       void    SPI_Init(SPI_RegDef_t* pSPI_Handle)
+*       void    SPI_DeInit(SPI_RegDef_t* pSPIx)
+*       void    SPI_PerClkCtrl(SPI_RegDef_t* pSPIx, uint8_t en_or_di)
+*       void    SPI_SendData(SPI_RegDef_t* pSPIx, uint8_t* pTxBuffer, uint32_t len)
+*       void    SPI_ReceiveData(SPI_RegDef_t* pSPIx, uint8_t* pRxBuffer, uint32_t len)
+*       void    SPI_IRQConfig(uint8_t IRQNumber, uint8_t en_or_di)
+*       void    SPI_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority)
+*       void    SPI_IRQHandling(SPI_Handle_t* pHandle)
 *
 **/
 
