998ffc1127d680ffcd13f5aee7f47e183efab578
maherme <manuel.hernandez.mendez@gmail.com>
2021-06-08 19:06:19 +0200

    Added SPI receive data API.

--- src/spi_driver.c
+++ src/spi_driver.c
@@ -151,6 +151,26 @@ void SPI_SendData(SPI_RegDef_t* pSPIx, uint8_t* pTxBuffer, uint32_t len){
 }
 
 void SPI_ReceiveData(SPI_RegDef_t* pSPIx, uint8_t* pRxBuffer, uint32_t len){
+
+        while(len > 0){
+        /* Wait until TXE is set */
+        while(SPI_GetFlagStatus(pSPIx, SPI_RXNE_FLAG) == FLAG_RESET);
+        /* Check the DFF bit in CR1 */
+        if((pSPIx->CR1 & (1 << SPI_CR1_DFF))){
+            /* 16 bit DFF */
+            /* Load the data from DR  to RxBuffer address*/
+            *((uint16_t*)pRxBuffer) = pSPIx->DR;
+            len--;
+            len--;
+            (uint16_t*)pRxBuffer++;
+        }
+        else{
+            /* 8 bit DFF */
+            *pRxBuffer = pSPIx->DR;
+            len--;
+            pRxBuffer++;
+        }
+    }
 }
 
 void SPI_IRQConfig(uint8_t IRQNumber, uint8_t en_or_di){
