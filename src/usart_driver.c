b25454807e38c8b6b88770ff006b2716de6e62f2
maherme <manuel.hernandez.mendez@gmail.com>
2021-06-15 00:39:05 +0200

    Added interrupt configuration API for USART.

--- src/usart_driver.c
+++ src/usart_driver.c
@@ -125,6 +125,41 @@ uint8_t USART_ReceiveDataIT(USART_Handle_t* pUSART_Handle, uint8_t* pRxBuffer, u
 }
 
 void USART_IRQConfig(uint8_t IRQNumber, uint8_t en_or_di){
+
+    if(en_or_di == ENABLE){
+        if(IRQNumber <= 31){
+            /* Program ISER0 register */
+            *NVIC_ISER0 |= (1 << IRQNumber);
+        }
+        else if(IRQNumber > 31 && IRQNumber < 64){
+            /* Program ISER1 register */
+            *NVIC_ISER1 |= (1 << (IRQNumber % 32));
+        }
+        else if(IRQNumber >= 64 && IRQNumber < 96){
+            /* Program ISER2 register */
+            *NVIC_ISER2 |= (1 << (IRQNumber % 64));
+        }
+        else{
+            /* do nothing */
+        }
+    }
+    else{
+        if(IRQNumber <= 31){
+            /* Program ICER0 register */
+            *NVIC_ICER0 |= (1 << IRQNumber);
+        }
+        else if(IRQNumber > 31 && IRQNumber < 64){
+            /* Program ICER1 register */
+            *NVIC_ICER1 |= (1 << (IRQNumber % 32));
+        }
+        else if(IRQNumber >= 64 && IRQNumber < 96){
+            /* Program ICER2 register */
+            *NVIC_ICER2 |= (1 << (IRQNumber % 64));
+        }
+        else{
+            /* do nothing */
+        }
+    }
 }
 
 void USART_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority){
