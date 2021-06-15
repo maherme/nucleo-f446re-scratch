f73c7deb02fdc34dd017b8ac8b60d316a8afb58e
maherme <manuel.hernandez.mendez@gmail.com>
2021-06-15 14:00:45 +0200

    Added bit position definition macros of control register for USART.

--- inc/stm32f446xx.h
+++ inc/stm32f446xx.h
@@ -420,6 +420,54 @@ typedef struct
 #define I2C_CCR_DUTY        14
 #define I2C_CCR_FS          15
 
+/**
+ * Bit position definition USART_CR1.
+ */
+#define USART_CR1_SBK       0
+#define USART_CR1_RWU       1
+#define USART_CR1_RE        2
+#define USART_CR1_TE        3
+#define USART_CR1_IDLEIE    4
+#define USART_CR1_RXNEIE    5
+#define USART_CR1_TCIE      6
+#define USART_CR1_TXEIE     7
+#define USART_CR1_PEIE      8
+#define USART_CR1_PS        9
+#define USART_CR1_PCE       10
+#define USART_CR1_WAKE      11
+#define USART_CR1_M         12
+#define USART_CR1_UE        13
+#define USART_CR1_OVER8     15
+
+/**
+ * Bit position definition USART_CR2.
+ */
+#define USART_CR2_ADD       0
+#define USART_CR2_LBDL      5
+#define USART_CR2_LBDIE     6
+#define USART_CR2_LBCL      8
+#define USART_CR2_CPHA      9
+#define USART_CR2_CPOL      10
+#define USART_CR2_CLKEN     11
+#define USART_CR2_STOP      12
+#define USART_CR2_LINEN     14
+
+/**
+ * Bit position definition USART_CR3.
+ */
+#define USART_CR3_EIE       0
+#define USART_CR3_IREN      1
+#define USART_CR3_IRLP      2
+#define USART_CR3_HDSEL     3
+#define USART_CR3_NACK      4
+#define USART_CR3_SCEN      5
+#define USART_CR3_DMAR      6
+#define USART_CR3_DMAT      7
+#define USART_CR3_RTSE      8
+#define USART_CR3_CTSE      9
+#define USART_CR3_CTSIE     10
+#define USART_CR3_ONEBIT    11
+
 /*****************************************************************************************************/
 /*          Peripheral definitions (peripheral base addresses typecasted to xxx_RegDef_t)            */
 /*****************************************************************************************************/
