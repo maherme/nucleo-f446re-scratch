26587c027e8ca89d998cfa4621617998a134a9bb
maherme <manuel.hernandez.mendez@gmail.com>
2022-09-03 20:59:52 +0200

    Add files for DMA driver

---
+++ inc/drv/dma_driver.h
@@ -0,0 +1,18 @@
+/********************************************************************************************************//**
+* @file dma_driver.h
+*
+* @brief Header file containing the prototypes of the APIs for configuring the DMA peripheral.
+*
+* Public Functions:
+*/
+
+#ifndef DMA_DRIVER_H
+#define DMA_DRIVER_H
+
+#include "stm32f446xx.h"
+
+/***********************************************************************************************************/
+/*                                       APIs Supported                                                    */
+/***********************************************************************************************************/
+
+#endif /* DMA_DRIVER_H */
---
+++ src/drv/dma_driver.c
@@ -0,0 +1,17 @@
+/********************************************************************************************************//**
+* @file dma_driver.c
+*
+* @brief File containing the APIs for configuring the DMA peripheral.
+*
+* Public Functions:
+*
+* @note
+*       For further information about functions refer to the corresponding header file.
+**/
+
+#include "dma_driver.h"
+
+/***********************************************************************************************************/
+/*                                       Public API Definitions                                            */
+/***********************************************************************************************************/
+
